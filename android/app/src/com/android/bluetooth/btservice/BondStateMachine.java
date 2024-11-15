de8b8c2489ffffec8a0f8fd0505b84e0e6cfcd67
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-15 12:31:26 +0800

    BondStateMachine: Fix crash during pairing confirmation
    test: m Bluetooth
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- android/app/src/com/android/bluetooth/btservice/BondStateMachine.java
+++ android/app/src/com/android/bluetooth/btservice/BondStateMachine.java
@@ -406,13 +406,20 @@ final class BondStateMachine extends StateMachine {
     };
 
     private boolean isSkipConfirmationAccessory(BluetoothDevice device) {
+        if (device == null || accConfirmSkip == null) {
+            return false;
+        }
+        String deviceName = device.getName();
+        BluetoothClass deviceClass = device.getBluetoothClass();
+        if (deviceName == null || deviceClass == null) {
+            return false;
+        }
         for (Pair<String, Integer> entry : accConfirmSkip) {
-            if (device.getName().equals(entry.first)
-                    && device.getBluetoothClass().getDeviceClass() == entry.second) {
+            if (Objects.equals(deviceName, entry.first)
+                    && deviceClass.getDeviceClass() == entry.second) {
                 return true;
             }
         }
-
         return false;
     }
 
